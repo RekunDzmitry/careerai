f017790c933a11560622029f1a8a72252c517334
RekunDzmitry <105891576+RekunDzmitry@users.noreply.github.com>
2024-03-09 14:06:42 +0100

    First fix of run.sh

--- bin/run copy.sh
+++ bin/run copy.sh
@@ -2,34 +2,34 @@
 file_path="/ui/next.config.js"
 echo "Current working directory: $(pwd)"
 
-# if [ -e "$file_path" ]; then
-#     echo "Next.js project already exists"
-# else
-#     npx create-next-app@13.4.12 $NEXTJS_PATH --typescript --tailwind --eslint --app --import-alias @/ --no-src-dir
-# fi
+if [ -e "$file_path" ]; then
+    echo "Next.js project already exists"
+else
+    npx create-next-app@13.4.12 $NEXTJS_PATH --typescript --tailwind --eslint --app --import-alias @/ --no-src-dir
+fi
 
-# ls -la
-# echo "Current working directory: $(pwd)"
-# cd $NEXTJS_PATH || { echo "Error changing directory"; exit 1; }
-# echo "Changed to directory: $(pwd)"
-# ls -la
-# npm i lucide-react
-# npm i @heroicons/react
-# npm i --save axios
+ls -la
+echo "Current working directory: $(pwd)"
+cd $NEXTJS_PATH || { echo "Error changing directory"; exit 1; }
+echo "Changed to directory: $(pwd)"
+ls -la
+npm i lucide-react
+npm i @heroicons/react
+npm i --save axios
 
-# # install shadcn-ui
-# file_path="/ui/components.json"
-# if [ -e "$file_path" ]; then
-#     echo "shadcn-ui was already initialized. Skipping..."
-# else
-#     echo "Initializing shadcn-ui..."
-#     yes '' | npx shadcn-ui@latest init
-#     echo "Fixing files..."
-#     python /bin/fix_nextjs_docker.py
-# fi
+# install shadcn-ui
+file_path="/ui/components.json"
+if [ -e "$file_path" ]; then
+    echo "shadcn-ui was already initialized. Skipping..."
+else
+    echo "Initializing shadcn-ui..."
+    yes '' | npx shadcn-ui@latest init
+    echo "Fixing files..."
+    python /bin/fix_nextjs_docker.py
+fi
 
-# npx shadcn-ui@latest add sheet -y
-# npx shadcn-ui@latest add button -y
-# npx shadcn-ui@latest add label -y
+npx shadcn-ui@latest add sheet -y
+npx shadcn-ui@latest add button -y
+npx shadcn-ui@latest add label -y
 
-# npm run dev -p 3001
+npm run dev -p 3001
