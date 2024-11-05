909a3ab164f8e7e7dcc665813dbd95cf52298aa5
RekunDzmitry <105891576+RekunDzmitry@users.noreply.github.com>
2024-11-05 17:49:32 +0100

    Remove /report from public route

--- frontend/ui/middleware.ts
+++ frontend/ui/middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
  
 export default authMiddleware({
-  publicRoutes: ["/", "/login", "/signup", "/report"]
+  publicRoutes: ["/", "/login", "/signup"]
 });
  
 export const config = {
